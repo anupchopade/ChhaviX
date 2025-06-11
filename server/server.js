d92a39381db871df57f18c20ec0077522bd09821
Anup Chopade <chopadeanup03@gamil.com>
2025-06-11 19:25:34 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-import 'dotenv'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
@@ -6,22 +5,15 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 const PORT=process.env.PORT || 4000
 const app=express()
 await connectDB()
-
 
 //middlewares init
 app.use(express.json())
 app.use(cors())
 
-
-
 app.get('/',(req,res)=>
 res.send("API working"))
-
-
-
 
 app.listen(PORT,()=>console.log("server ruuning on port " + PORT))
