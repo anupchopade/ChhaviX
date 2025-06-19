b79141893bdff9bcb5ec2297f9a341c482d90556
Anup Chopade <chopadeanup03@gamil.com>
2025-06-20 01:42:58 +0530

    try

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,130 +1,130 @@
-// import {Webhook} from 'svix'
-// import userModel from '../models/userModel.js'
-// //api controller fun to manage clerk user with databse
-// //http://localhost:4000/api/user/webhooks
+import {Webhook} from 'svix'
+import userModel from '../models/userModel.js'
+//api controller fun to manage clerk user with databse
+//http://localhost:4000/api/user/webhooks
 
-// const clerkWebhooks=async (req,res)=>{
+const clerkWebhooks=async (req,res)=>{
     
 
 
-//     try{
+    try{
 
 
-//         const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-//         await whook.verify(JSON.stringify(req.body),{
-//             "svix-id":req.headers["svix-id"],
-//             "svix-timestamp":req.headers["svix-timestamp"],
-//             "svix-signature":req.headers["svix-signature"]
-//         })
+        const whook=new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+        await whook.verify(JSON.stringify(req.body),{
+            "svix-id":req.headers["svix-id"],
+            "svix-timestamp":req.headers["svix-timestamp"],
+            "svix-signature":req.headers["svix-signature"]
+        })
 
-//         const {data, type}=req.body
-//         console.log('Webhook received:', { type, data });
-
-//         switch (type) {
-//             case "user.created":{
-//                 const userData={
-//                     clerkId:data.id,
-//                     email:data.email_addresses[0].email_address,
-//                     firstName:data.first_name,
-//                     lastName:data.last_name,
-//                     photo:data.image_url
-
-//                 }
-
-//                 await userModel.create(userData)
-//                 res.json({})
-               
-//                 break;
-//             }
-//             case "user.updated":{
-//                 const userData={
-//                     email:data.email_addresses[0].email_address,
-//                     firstName:data.first_name,
-//                     lastName:data.last_name,
-//                     photo:data.image_url
-
-//                 }
-//                 await userModel.findOneAndUpdate({clerkId:data.id},userData)
-//                 res.json({})
-//                 break;
-//             }
-//             case "user.deleted":{
-//                 await userModel.findOneAndDelete({clerkId:data.id})
-//                 res.json({})
-
-//                 break;
-//             }
-//             default:
-//                 break;
-//         }
-
-//     }
-//     catch(error){
-//         console.log(error.message)
-//         res.json({success:false,message:error.message})
-//     }
-// }
-
-// export {clerkWebhooks}
-
-
-import { Webhook } from 'svix'
-import userModel from '../models/userModel.js'
-
-const clerkWebhooks = async (req, res) => {
-    try {
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-
-        const payload = req.body.toString('utf8')  // 🔥 This is the critical fix
-        const headers = {
-            "svix-id": req.headers["svix-id"],
-            "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix-signature"]
-        }
-
-        const evt = whook.verify(payload, headers)
-        const { data, type } = evt
-
-        console.log('Webhook received:', { type, data })
+        const {data, type}=req.body
+        console.log('Webhook received:', { type, data });
 
         switch (type) {
-            case "user.created": {
-                const userData = {
-                    clerkId: data.id,
-                    email: data.email_addresses[0].email_address,
-                    firstName: data.first_name,
-                    lastName: data.last_name,
-                    photo: data.image_url
+            case "user.created":{
+                const userData={
+                    clerkId:data.id,
+                    email:data.email_addresses[0].email_address,
+                    firstName:data.first_name,
+                    lastName:data.last_name,
+                    photo:data.image_url
+
                 }
 
                 await userModel.create(userData)
                 res.json({})
-                break
+               
+                break;
             }
-            case "user.updated": {
-                const userData = {
-                    email: data.email_addresses[0].email_address,
-                    firstName: data.first_name,
-                    lastName: data.last_name,
-                    photo: data.image_url
+            case "user.updated":{
+                const userData={
+                    email:data.email_addresses[0].email_address,
+                    firstName:data.first_name,
+                    lastName:data.last_name,
+                    photo:data.image_url
+
                 }
-                await userModel.findOneAndUpdate({ clerkId: data.id }, userData)
+                await userModel.findOneAndUpdate({clerkId:data.id},userData)
                 res.json({})
-                break
+                break;
             }
-            case "user.deleted": {
-                await userModel.findOneAndDelete({ clerkId: data.id })
+            case "user.deleted":{
+                await userModel.findOneAndDelete({clerkId:data.id})
                 res.json({})
-                break
+
+                break;
             }
             default:
-                break
+                break;
         }
 
-    } catch (error) {
-        console.log("Webhook error:", error.message)
-        res.status(400).json({ success: false, message: error.message })
+    }
+    catch(error){
+        console.log(error.message)
+        res.json({success:false,message:error.message})
     }
 }
 
-export { clerkWebhooks }
+export {clerkWebhooks}
+
+
+// import { Webhook } from 'svix'
+// import userModel from '../models/userModel.js'
+
+// const clerkWebhooks = async (req, res) => {
+//     try {
+//         const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
+
+//         const payload = req.body.toString('utf8')  // 🔥 This is the critical fix
+//         const headers = {
+//             "svix-id": req.headers["svix-id"],
+//             "svix-timestamp": req.headers["svix-timestamp"],
+//             "svix-signature": req.headers["svix-signature"]
+//         }
+
+//         const evt = whook.verify(payload, headers)
+//         const { data, type } = evt
+
+//         console.log('Webhook received:', { type, data })
+
+//         switch (type) {
+//             case "user.created": {
+//                 const userData = {
+//                     clerkId: data.id,
+//                     email: data.email_addresses[0].email_address,
+//                     firstName: data.first_name,
+//                     lastName: data.last_name,
+//                     photo: data.image_url
+//                 }
+
+//                 await userModel.create(userData)
+//                 res.json({})
+//                 break
+//             }
+//             case "user.updated": {
+//                 const userData = {
+//                     email: data.email_addresses[0].email_address,
+//                     firstName: data.first_name,
+//                     lastName: data.last_name,
+//                     photo: data.image_url
+//                 }
+//                 await userModel.findOneAndUpdate({ clerkId: data.id }, userData)
+//                 res.json({})
+//                 break
+//             }
+//             case "user.deleted": {
+//                 await userModel.findOneAndDelete({ clerkId: data.id })
+//                 res.json({})
+//                 break
+//             }
+//             default:
+//                 break
+//         }
+
+//     } catch (error) {
+//         console.log("Webhook error:", error.message)
+//         res.status(400).json({ success: false, message: error.message })
+//     }
+// }
+
+// export { clerkWebhooks }
--- server/server.js
+++ server/server.js
@@ -1,45 +1,3 @@
-// import express from 'express'
-// import cors from 'cors'
-// import connectDB from './configs/mongodb.js'
-// import userRouter from './routes/userRoutes.js'
-// import dotenv from 'dotenv';
-// import bodyParser from 'body-parser'  // <-- ADD THIS
-
-// dotenv.config();
-
-// const app = express()
-
-// //middlewares init
-// app.use(express.json())
-// app.use(cors())
-
-// app.get('/',(req,res)=>
-// res.send("API working"))
-
-
-// //added on suggestion
-// app.use('/api/user/webhooks', bodyParser.raw({ type: 'application/json' })) 
-
-
-
-// app.use('/api/user',userRouter)
-
-// // Connect to MongoDB
-// connectDB().catch(console.error)
-
-// // For local development
-// if (process.env.NODE_ENV !== 'production') {
-//   const PORT = process.env.PORT || 4000
-//   app.listen(PORT, () => console.log("server running on port " + PORT))
-// }
-
-
-// // Export the Express API
-// export default app
-
-
-
-
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
@@ -51,23 +9,60 @@ dotenv.config()
 
 const app = express()
 
-// ✅ Clerk webhook route should use raw body
+// ✅ Clerk webhook MUST use raw body, put this BEFORE express.json()
 app.use('/api/user/webhooks', bodyParser.raw({ type: 'application/json' }))
 
-// ✅ other middlewares after webhook setup
+// ✅ Other middleware
 app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => res.send("API working"))
 
+// ✅ Routes
 app.use('/api/user', userRouter)
 
-// connect to DB
+// ✅ DB connect
 connectDB().catch(console.error)
 
+// ✅ Server start
 if (process.env.NODE_ENV !== 'production') {
     const PORT = process.env.PORT || 4000
     app.listen(PORT, () => console.log("server running on port " + PORT))
 }
 
-export default app  
+export default app
+
+
+
+
+// import express from 'express'
+// import cors from 'cors'
+// import connectDB from './configs/mongodb.js'
+// import userRouter from './routes/userRoutes.js'
+// import dotenv from 'dotenv'
+// import bodyParser from 'body-parser'
+
+// dotenv.config()
+
+// const app = express()
+
+// // ✅ Clerk webhook route should use raw body
+// app.use('/api/user/webhooks', bodyParser.raw({ type: 'application/json' }))
+
+// // ✅ other middlewares after webhook setup
+// app.use(express.json())
+// app.use(cors())
+
+// app.get('/', (req, res) => res.send("API working"))
+
+// app.use('/api/user', userRouter)
+
+// // connect to DB
+// connectDB().catch(console.error)
+
+// if (process.env.NODE_ENV !== 'production') {
+//     const PORT = process.env.PORT || 4000
+//     app.listen(PORT, () => console.log("server running on port " + PORT))
+// }
+
+// export default app  
